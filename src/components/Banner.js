71d4b4754e21ef57dba245360af3b876a9f3c901
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-07 02:38:20 +0300

    React project and Navbar Component Created...

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import {Container, Row, Col} from "react-bootstrap";
+
+export const Banner = () => {
+  return (
+    <section className='banner' id='home'>
+        <Container>
+            <Row className="align-items-center">
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+      
+    </section>
+  )
+}
+
+export default Banner
