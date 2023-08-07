8d89263ceb650bf6908d1db78b1a65c6eb7c86ed
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-07 18:27:12 +0300

    Projects component added...

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,5 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { Container, Row, Col } from "react-bootstrap";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
